b8cb769ded0f01231d4f58d39d4f958d0b23fa4c
jujemu <jujemu30@gmail.com>
2023-03-29 00:51:53 +0900

    What is 'props'?

--- run.js
+++ run.js
@@ -8,14 +8,9 @@ function Header(props){
 } 
 
 function Nav(props){
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read'+t.id}>{t.title}</a></li>)
-  }
   return <nav>
   <ol>
-    {lis}
+    {props.topics}
   </ol>
 </nav>
 }
